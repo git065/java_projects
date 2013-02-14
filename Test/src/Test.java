b7f2d1ee5a3b60f4f302167c977b6021c86f5ca8
deathkid777 <deathkid777@gmail.com>
2013-02-14 14:52:38 +0300

    upd

---
+++ Test/src/Test.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author A.Kumpel
+ *
+ */
+public class Test {
+
+	/**
+	 * 
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
