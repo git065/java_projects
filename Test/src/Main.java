70cfbcf9c6f3897c03ccc823cf07dc0ee88f45cd
deathkid777 <deathkid777@gmail.com>
2013-03-05 22:15:53 +0400

    upd

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,14 +1,34 @@
 public class Main {
 	String variable;
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		System.out.println("Hello World!");
 		B b = new B();
+	}*/
+	
+	public enum Members {JERRY, BOBBY, PHIL};
+	
+	public static void main(String[] args) {
+		Members n = Members.BOBBY;		
+		Members ifName = Members.PHIL;
+		switch (ifName) {
+		case PHIL:
+			System.out.println("1");
+		case BOBBY:
+			System.out.println("2");
+		case JERRY:
+			System.out.println("3");
+		}
+		//System.out.println("Hello World!");
+		//B b = new B();
 	}
 
 	public Main() {
 		System.out.println("Constructor A");
 		printVariable();
+		Object a = new float[2];
+		//short[] x2 = [5];
+		char c = 49;
 	}
 
 	protected void printVariable() {
